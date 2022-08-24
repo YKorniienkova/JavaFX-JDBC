6bcb1d3b012aeaa72e6de01aa269f23756473c79
YKorniienkova <lkornejka@gmail.com>
2022-08-24 22:58:57 +0200

    add new controller

---
+++ src/main/java/com/petproject/controllers/RegulationsController.java
@@ -0,0 +1,46 @@
+package com.petproject.controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class RegulationsController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button backb;
+
+    @FXML
+    void initialize() {
+       backb.setOnAction(event->{
+           backb.getScene().getWindow().hide();
+
+           FXMLLoader loader = new FXMLLoader();
+           loader.setLocation(getClass().getResource("/com/petproject/game.fxml"));
+
+           try {
+               loader.load();
+           } catch (IOException e) {
+               throw new RuntimeException(e);
+           }
+           Parent root = loader.getRoot();
+           Stage stage = new Stage();
+           stage.setScene(new Scene(root));
+           stage.show();
+       });
+
+    }
+
+}
